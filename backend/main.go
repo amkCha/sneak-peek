f3fb199aacaab15026bdf77108099a5efb3c92b6
christian.tran <christian.trn@gmail.com>
2021-06-10 12:37:00 +0200

    add backend

---
+++ backend/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/ChatelainSys/SneakPeek/backend/db"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func InitDB() *db.DB {
+	addr := os.Getenv("DB_ADDRESS")
+	username := os.Getenv("DB_USERNAME")
+	password := os.Getenv("DB_PASSWORD")
+
+	opt := options.Client().ApplyURI(fmt.Sprintf("mongodb+srv://%s:%s@%s?retryWrites=true&w=majority", username, password, addr))
+	d, err := db.NewClient("sneak-peek", opt)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return d
+}
+
+type Handlers struct {
+	DB *db.DB
+}
+
+func (h *Handlers) buildProof(c *gin.Context) {
+
+	var json struct {
+		TokenAddress  string `json:"tokenAddress" binding:"required"`
+		WalletAddress string `json:"walletAddress" binding:"required"`
+	}
+
+	if c.Bind(&json) == nil {
+		res, _ := h.DB.GetIntraDayTrades(context.Background())
+		log.Printf("%v\n", res)
+		c.JSON(http.StatusOK, gin.H{"proof": "ok"})
+	}
+}
+
+func main() {
+	r := gin.Default()
+
+	hand := &Handlers{DB: InitDB()}
+
+	r.POST("/build-proof", hand.buildProof)
+
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
