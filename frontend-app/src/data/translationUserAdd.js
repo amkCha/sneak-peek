dbef4d8808d7362f9603322db8a6ab09d93a3fc3
amKCha <akc2138@caa.columbia.edu>
2021-06-14 21:55:50 +0200

    add address

--- frontend-app/src/data/translationUserAdd.js
+++ frontend-app/src/data/translationUserAdd.js
@@ -1,3 +1,4 @@
 export const AddressToUsername = {
-  "0x2844d58B9E822a7159d7dc38Ed265a459a712a55": "Amel"
+  "0x2844d58B9E822a7159d7dc38Ed265a459a712a55": "Amel",
+  "0x7E654d251Da770A068413677967F6d3Ea2FeA9E4" : "Nico"
 };
